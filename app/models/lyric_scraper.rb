96df64de1924da0101c4d7bb6f69ac0fa7f200f2
Dave Kaiser <email": null>
2016-04-28 18:16:59 -0500

    add partially working scraper

---
+++ app/models/lyric_scraper.rb
@@ -0,0 +1,38 @@
+require 'nokogiri'
+require 'open-uri'
+
+google_url = "https://www.google.com/?gws_rd=ssl#q=lyrics"
+
+song = "born to run"
+song_words = song.split(' ')
+
+song_words.each do |word|
+  google_url += "+" + word
+end
+
+html = open(google_url)
+ndoc = Nokogiri.parse(html)
+puts ndoc
+
+# url = "https://play.google.com/music/preview/Tnlwgpr4rl62zblmhhowiep653a?lyrics=1&utm_source=google&utm_medium=search&utm_campaign=lyrics&pcampaignid=kp-lyrics"
+
+
+# html = open(url)
+# nokogiri_document = Nokogiri.parse(html)
+
+# lyrics = nokogiri_document.css('.lyrics > p').to_s
+
+# def scrub_tags(text)
+#  text.gsub!(/<.{1,3}>/,' ')
+# end
+
+# puts scrub_tags(lyrics)
+# puts lyrics
+
+#use nokogiri to search google
+#then find the a tag around "full lyrics on google play"
+#then go there and scrape the lyrics!
+
+
+
+
